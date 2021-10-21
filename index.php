c70f8db3f84f90b09bb03af6ccb3abb87093d860
dinhhieu <dinhhieu210899@gmail.com>
2021-10-22 02:44:04 +0700

    web

---
+++ index.php
@@ -0,0 +1,37 @@
+<!-- wp:paragraph -->
+<p><strong>Bạn đã biết cách thiết lập chụp ảnh từ xa bằng lời nói trên&nbsp;<a href="https://cellphones.com.vn/mobile/apple.html">iPhone</a>&nbsp;với Siri, giúp những tấm hình chụp cá nhân hay chụp nhóm trở nên dễ hơn bao giờ hết mà không cần phải thực hiện hẹn giờ phức tạp. Bài viết sau đây sẽ hướng dẫn các bạn từng bước thực hiện việc này.</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul><li><a href="https://cellphones.com.vn/sforum/nhung-thiet-lap-iphone-can-ban-giup-nguoi-lon-tuoi-de-su-dung-1">Những thiết lập iPhone căn bản giúp người lớn tuổi dễ sử dụng #1</a></li><li><a href="https://cellphones.com.vn/sforum/huong-dan-khoa-tab-an-danh-google-chrome-tren-iphone">Hướng dẫn khóa tab ẩn danh Google Chrome trên iPhone</a></li><li><a href="https://cellphones.com.vn/sforum/cach-khac-phuc-khi-man-hinh-cam-ung-khong-nhay" target="_blank" rel="noreferrer noopener">Cách khắc phục khi màn hình cảm ứng không nhạy</a></li></ul>
+<!-- /wp:list -->
+
+<!-- wp:paragraph -->
+<p>Ngày nay, việc chụp ảnh trở thành việc không thể thiếu với mọi người. Ghi lại những tấm hình đẹp sẽ giúp chúng ta lưu giữ lại những khoảnh khắc ý nghĩa cùng bạn bè, người thân. Tuy nhiên, khi sử dụng tripod để tự chụp những tấm hình với phong cảnh phía sau, hay tạo những pose ảnh đẹp cũng “đồng bọn” trong những chuyến du lịch, ta thường phải canh và sử dụng tính năng hẹn giờ khá phiền phức? Vậy tại sao chúng ta không giải quyết vấn đề bằng cách “nhờ Siri” chụp dùm chúng ta nhỉ? Sau đây là cách chụp ảnh từ xa bằng trợ lý ảo Siri, mời các bạn cùng theo dõi.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"align":"center","id":477332} -->
+<div class="wp-block-image"><figure class="aligncenter"><img src="https://cellphones.com.vn/sforum/wp-content/uploads/2021/10/BG-11-scaled.jpg" alt="Sforum - Trang thông tin công nghệ mới nhất BG-11-scaled Mẹo điều khiển chụp ảnh từ xa trên iPhone bằng giọng nói cực đơn giản " class="wp-image-477332" title="Sforum - Trang thông tin công nghệ mới nhất BG-11-scaled Mẹo điều khiển chụp ảnh từ xa trên iPhone bằng giọng nói cực đơn giản "/></figure></div>
+<!-- /wp:image -->
+
+<!-- wp:paragraph -->
+<p>Mục lục<a></a></p>
+<!-- /wp:paragraph -->
+<video controls>
+  <source src="./video/abc.mp4" type="video/mp4">
+</video>
+<!-- wp:list -->
+<ul><li><a href="https://cellphones.com.vn/sforum/meo-dieu-khien-chup-anh-tu-xa-tren-iphone-bang-giong-noi-cuc-don-gian#Huong_dan_chup_anh_tu_xa_voi_Siri">Hướng dẫn chụp ảnh từ xa với Siri</a></li><li><a href="https://cellphones.com.vn/sforum/meo-dieu-khien-chup-anh-tu-xa-tren-iphone-bang-giong-noi-cuc-don-gian#Tam_ket">Tạm kết:</a></li></ul>
+<!-- /wp:list -->
+
+<!-- wp:heading -->
+<h2><strong>Hướng dẫn chụp ảnh từ xa với Siri</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><em><strong>Điều đầu tiên, bạn cần phải có ứng dụng Shortcuts (Phím tắt) trên chiếc điện thoại iPhone của bạn. Đây là ứng dụng mặc định có sẵn trên máy. Tuy nhiên nếu bạn đã lỡ xóa thì bạn hoàn toàn có thể tải lại bằng cách&nbsp;<a href="https://apps.apple.com/us/app/shortcuts/id915249334" target="_blank" rel="noreferrer noopener">click tại đây.</a></strong></em></p>
+<!-- /wp:paragraph -->
